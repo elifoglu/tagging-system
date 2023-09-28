c07df27c049c23b66a8971293795321668a29e8a
mert <elifoglumert@gmail.com>
2023-09-29 01:23:47 +0300

    replace left frame view of tag page with a tree view

--- tagging-system/src/Component/Page/Tag/View.elm
+++ tagging-system/src/Component/Page/Tag/View.elm
@@ -5,14 +5,14 @@ import App.Msg exposing (Msg(..))
 import Html exposing (Html, a, b, br, div, img, span, text)
 import Html.Attributes exposing (class, href, src, style)
 import Tag.Model exposing (Tag)
-import Tag.Util exposing (tagByIdForced)
 import Tag.TagTextUtil exposing (viewTagText)
+import Tag.Util exposing (tagByIdForced)
 import TagInfoIcon.View exposing (viewTagInfoIcon)
 
 
 viewTagPageDiv : InitializedTagPageModel -> List Tag -> Html Msg
 viewTagPageDiv initialized allTags =
-    div [ style "margin-top" "27px"]
+    div [ style "margin-top" "27px" ]
         [ viewLeftFrame initialized allTags
         , viewRightFrame initialized
         ]
@@ -21,27 +21,35 @@ viewTagPageDiv initialized allTags =
 viewLeftFrame : InitializedTagPageModel -> List Tag -> Html Msg
 viewLeftFrame initialized allTags =
     div [ class "leftFrameOnTagPage leftFrameTagsFont" ]
-        [ div [ style "margin-left" "20px", style "font-size" "14px" ] [ b [] [ text ("#" ++ initialized.tag.name) ] ]
-        , viewTagsDiv initialized.tag.parentTags allTags "/up.png"
-        , viewTagsDiv initialized.tag.childTags allTags "/down.png"
+        [ viewTagsDiv initialized.tag.parentTags allTags Parent
+        , div
+            [ style "font-size" "14px"
+            , style "padding-left"
+                (if not (List.isEmpty initialized.tag.parentTags) then
+                    "5px"
+
+                 else
+                    "0px"
+                )
+            ]
+            [ b [] [ text ("#" ++ initialized.tag.name) ] ]
+        , viewTagsDiv initialized.tag.childTags allTags Child
         ]
 
 
-viewTagsDiv : List String -> List Tag -> String -> Html Msg
-viewTagsDiv tagIds allTags iconSrc =
-    if List.length tagIds == 0 then
-        text " "
+type TagType
+    = Parent
+    | Child
 
-    else
-        div [ class "tagsDiv" ]
-            [ div [ class "tagsDivChild tagsDivFirstChild" ] [ img [ class "upAndDownIcons", src iconSrc ] [] ]
-            , div [ class "tagsDivChild" ]
-                (tagIds
-                    |> List.map (tagByIdForced allTags)
-                    |> List.map viewTag
-                    |> List.intersperse (br [] [])
-                )
-            ]
+
+viewTagsDiv : List String -> List Tag -> TagType -> Html Msg
+viewTagsDiv tagIds allTags tagType =
+    div []
+        (tagIds
+            |> List.map (tagByIdForced allTags)
+            |> List.map (viewTag tagType)
+            |> List.intersperse (br [] [])
+        )
 
 
 viewRightFrame : InitializedTagPageModel -> Html Msg
@@ -50,11 +58,20 @@ viewRightFrame initialized =
         [ viewTagText initialized.tag initialized.textParts
         ]
 
-viewTag : Tag -> Html Msg
-viewTag tag =
+
+viewTag : TagType -> Tag -> Html Msg
+viewTag tagType tag =
     span []
         [ a
             [ class "tagPageTagA"
+            , style "padding-left"
+                (case tagType of
+                    Parent ->
+                        "0px"
+
+                    Child ->
+                        "10px"
+                )
             , href
                 ("/tags/"
                     ++ tag.tagId
