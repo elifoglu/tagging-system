d3f65da3b16cfa6eaee659e0ad98743544d7c937
mert <elifoglumert@gmail.com>
2023-10-05 03:32:59 +0300

    make tag selectors look much better

--- tagging-system/src/Component/Molecule/TagPicker/View.elm
+++ tagging-system/src/Component/Molecule/TagPicker/View.elm
@@ -11,11 +11,20 @@ import TypedSvg.Events exposing (onFocusOut)
 
 getTagOptionsToShow : TagPickerModuleModel -> List TagOption
 getTagOptionsToShow model =
-    if model.showTagOptionList == False then []
+    if model.showTagOptionList == False then
+        []
+
     else
         model.allAvailableTagOptions
             |> List.filter (removeFilteredThingFirst model.tagIdToFilterOut)
-            |> List.filter (\a -> if model.input == "" then True else String.contains model.input a.tagName)
+            |> List.filter
+                (\a ->
+                    if model.input == "" then
+                        True
+
+                    else
+                        String.contains model.input a.tagName
+                )
             |> List.filter (\a -> not (List.member a model.selectedTagOptions))
 
 
@@ -33,8 +42,10 @@ viewTagPickerDiv : TagPickerModuleModel -> WorkingOnWhichModule -> Html Msg
 viewTagPickerDiv tagPickerModel workingOnWhichModule =
     div [] <|
         [ viewInput "text" "search tags..." tagPickerModel.input (TagPickerModuleInputChanged workingOnWhichModule ToggleSelectionList) (tagCreateOrUpdateInputMessage workingOnWhichModule SearchInput) (KeyDown (TagPickerModuleInput workingOnWhichModule))
-        , viewSelecteds tagPickerModel (tagCreateOrUpdateInputMessage workingOnWhichModule OptionRemoved)
-        , viewSelectInput tagPickerModel (tagCreateOrUpdateInputMessage workingOnWhichModule OptionClicked)
+        , div [ class "baseTagPickerDiv" ]
+            [ viewSelectedTagOptions tagPickerModel (tagCreateOrUpdateInputMessage workingOnWhichModule OptionRemoved)
+            , viewTagOptionsToSelect tagPickerModel (tagCreateOrUpdateInputMessage workingOnWhichModule OptionClicked)
+            ]
         ]
 
 
@@ -45,16 +56,16 @@ tagCreateOrUpdateInputMessage workingOnWhichModule a b =
 
 viewInput : String -> String -> String -> msg -> (String -> msg) -> (KeyDownType -> msg) -> Html msg
 viewInput t p v focusMsg inputMsg keydownMsg =
-    input [ type_ t, placeholder p, value v, selected True, onInput inputMsg, onClick focusMsg, onKeyDown keydownMsg,  style "width" "100px" ] []
+    input [ type_ t, placeholder p, value v, selected True, onInput inputMsg, onClick focusMsg, onKeyDown keydownMsg, style "width" "100px" ] []
 
 
-viewSelecteds : TagPickerModuleModel -> (TagOption -> msg) -> Html msg
-viewSelecteds model toMsg =
+viewSelectedTagOptions : TagPickerModuleModel -> (TagOption -> msg) -> Html msg
+viewSelectedTagOptions model toMsg =
     if List.isEmpty model.selectedTagOptions then
         text ""
 
     else
-        div []
+        div [ class "selectedTagOptionsDiv" ]
             (model.selectedTagOptions
                 |> List.map (selectedTagOptionDiv toMsg)
             )
@@ -62,23 +73,23 @@ viewSelecteds model toMsg =
 
 selectedTagOptionDiv : (TagOption -> msg) -> TagOption -> Html msg
 selectedTagOptionDiv toMsg tagOption =
-    div [ class "dropdown-content1", onClick (toMsg tagOption) ]
+    div [ class "selectedTagOptionDiv", onClick (toMsg tagOption) ]
         [ div [ class "selectedTagOptionWithX" ]
             [ span [ class "selectedTagOption" ]
-                [ text tagOption.tagName ]
+                [ text ("#" ++ tagOption.tagName) ]
             , span [ class "hide" ]
-                [ text "X" ]
+                [ text "x" ]
             ]
         ]
 
 
-viewSelectInput : TagPickerModuleModel -> (TagOption -> msg) -> Html msg
-viewSelectInput model toMsg =
+viewTagOptionsToSelect : TagPickerModuleModel -> (TagOption -> msg) -> Html msg
+viewTagOptionsToSelect model toMsg =
     if List.isEmpty (getTagOptionsToShow model) then
         text ""
 
     else
-        div [ class "dropdown-content2" ]
+        div [ class "tagOptionsToSelectDiv" ]
             (getTagOptionsToShow model
                 |> List.map (viewOption toMsg)
             )
@@ -86,6 +97,6 @@ viewSelectInput model toMsg =
 
 viewOption : (TagOption -> msg) -> TagOption -> Html msg
 viewOption toMsg tagOption =
-    div [ class "tagOptionToSelect", onClick (toMsg tagOption) ]
-        [ text tagOption.tagName
+    div [ class "tagOptionToSelectDiv", onClick (toMsg tagOption) ]
+        [ text ("#" ++ tagOption.tagName)
         ]
